20f7ff87924cbdafd2dc43c1ca6133a130862867
bonggyeong <iampbk7@gmail.com>
2025-07-09 17:46:33 +0900

    links

--- activate_rt_servo.sh
+++ activate_rt_servo.sh
@@ -7,7 +7,10 @@ rosservice call /$ROBOT_NS/system/set_safety_mode "{safety_mode: 1, safety_mode_
 
 # RT: https://github.com/DoosanRobotics/doosan-robot/issues/207
 # 7056: https://github.com/DoosanRobotics/doosan-robot/issues/128
-rosservice call /$ROBOT_NS/realtime/connect_rt_control "{ip_address: '192.168.137.100', port: 12345}"
+# rosservice call /$ROBOT_NS/realtime/connect_rt_control "{ip_address: '192.168.137.100', port: 12345}"
+# port: https://github.com/DoosanRobotics/doosan-robot/issues/107
+rosservice call /$ROBOT_NS/realtime/connect_rt_control "ip_address: '192.168.137.100'"
+
 rosservice call /$ROBOT_NS/realtime/set_rt_control_output "{version: 'v1.0', period: 0.1, loss: 4}"
 rosservice call /$ROBOT_NS/realtime/start_rt_control
 ...
